e87e87eb6cc1fa0336ac92383bb3e46d327a6ae5
Bruno Rolim <brolim@gmail.com>
2012-03-29 01:02:01 -0300

    erasing small tests that were testing by others

--- spec/jquery_tree_spec.js
+++ spec/jquery_tree_spec.js
@@ -1,41 +1,33 @@
 describe("JQueryTree", function() {
 
+  beforeEach(function () {
+    root1 = {name: 'node1', children: [{name:'node1.1', children: [{name:'node1.1.1'}, {name:'node1.1.2'}]}, 
+                                       {name:'node1.2'}]},
+    root2 = {name: 'node2', children: [{name:'node2.1'}, {name:'node2.2'}]}
+    root3 = {name: 'node3', children: [{name:'node3.1'}, {name:'node3.2'}]}
+    tree = new JQueryTree([root1, root2, root3]);
+  });
+
   describe("rendering", function() {
     
-    it("renders the root div", function() {
-      tree = new JQueryTree([{name: 'name1'}]);
-      expect(tree.el).toContain('<div class="root">');
-    });
-
-    it("renders the root div with its name", function() {
-      tree = new JQueryTree([{name: 'name1'}]);
-      expect($(tree.el).text()).toBe('name1');
-    });
-
     it("renders roots and children nodes", function() {
-      root1 = {name: 'node1', children: [{name:'node1.1', children: [{name:'node1.1.1'}, {name:'node1.1.2'}]}, 
-                                         {name:'node1.2'}]},
-      root2 = {name: 'node2', children: [{name:'node2.1'}, {name:'node2.2'}]}
-      root3 = {name: 'node3', children: [{name:'node3.1'}, {name:'node3.2'}]}
-      tree = new JQueryTree([root1, root2, root3]);
-
       expect(tree.roots().length).toBe(3);
       expect(tree.nodes(0).length).toBe(3); // tree.roots() is the same
-      expect(tree.roots().eq(0).find('span:eq(0)').text()).toBe('node1');
-      expect(tree.roots().eq(1).find('span:eq(0)').text()).toBe('node2');
-      expect(tree.roots().eq(2).find('span:eq(0)').text()).toBe('node3');
+      expect(tree.roots().eq(0).find('.name:eq(0)').text()).toBe('node1');
+      expect(tree.roots().eq(1).find('.name:eq(0)').text()).toBe('node2');
+      expect(tree.roots().eq(2).find('.name:eq(0)').text()).toBe('node3');
 
       expect(tree.nodes(1).length).toBe(6);
-      expect(tree.nodes(1).eq(0).find('span:eq(0)').text()).toBe('node1.1');
-      expect(tree.nodes(1).eq(1).find('span:eq(0)').text()).toBe('node1.2');
-      expect(tree.nodes(1).eq(2).find('span:eq(0)').text()).toBe('node2.1');
-      expect(tree.nodes(1).eq(3).find('span:eq(0)').text()).toBe('node2.2');
-      expect(tree.nodes(1).eq(4).find('span:eq(0)').text()).toBe('node3.1');
-      expect(tree.nodes(1).eq(5).find('span:eq(0)').text()).toBe('node3.2');
+      expect(tree.nodes(1).eq(0).find('.name:eq(0)').text()).toBe('node1.1');
+      expect(tree.nodes(1).eq(1).find('.name:eq(0)').text()).toBe('node1.2');
+      expect(tree.nodes(1).eq(2).find('.name:eq(0)').text()).toBe('node2.1');
+      expect(tree.nodes(1).eq(3).find('.name:eq(0)').text()).toBe('node2.2');
+      expect(tree.nodes(1).eq(4).find('.name:eq(0)').text()).toBe('node3.1');
+      expect(tree.nodes(1).eq(5).find('.name:eq(0)').text()).toBe('node3.2');
 
       expect(tree.nodes(2).length).toBe(2);
-      expect(tree.nodes(2).eq(0).find('span:eq(0)').text()).toBe('node1.1.1');
-      expect(tree.nodes(2).eq(1).find('span:eq(0)').text()).toBe('node1.1.2');
+      expect(tree.nodes(2).eq(0).find('.name:eq(0)').text()).toBe('node1.1.1');
+      expect(tree.nodes(2).eq(1).find('.name:eq(0)').text()).toBe('node1.1.2');
 
       expect(tree.nodes(3).length).toBe(0);
     });
